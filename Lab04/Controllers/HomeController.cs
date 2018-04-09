4e496359da600463a721f1b7d5a001ded6c3f519
heumanl <heumanl@go.stockton.edu>
2018-04-09 14:18:46 -0400

    Add project files.

---
+++ Lab04/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab04.Models;
+
+namespace Lab04.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            return Redirect("Lab4");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab04/Controllers/Lab4Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab04.Controllers
+{
+    public class Lab4Controller : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var myTime = DateTime.Now;
+        //    var myDate = DateTime.Now.ToString("D");
+
+            //View
+
+            return View(myTime);
+        }
+    }
+}
